baeae9386b656f0a0dc20332e029757fd2d612a3
Garudhiya <haikal@garudhiya.com>
2025-10-28 11:10:42 +0500

    Rename loadPosts to loadBlogCards and add details

--- assets/js/blogs.js
+++ assets/js/blogs.js
@@ -10,7 +10,7 @@ const posts = [
     }
 ];
 
-function loadPosts() {
+function loadBlogCards() {
     const main = document.querySelector('main');
     
     posts.forEach(post => {
@@ -20,12 +20,35 @@ function loadPosts() {
             <img src="${post.img}" alt="${post.title}">
             <p>${post.excerpt}</p>
             <p>By ${post.author} on ${post.date}</p>
-            <p>${post.content}</p>
+            <a href="blog-details.html?id=${post.id}">Read More</a>
         `;
         main.appendChild(article);
     });
 }
 
+function loadBlogDetails() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const postId = parseInt(urlParams.get('id'));
+    
+    const post = posts.find(p => p.id === postId);
+    
+    if (post) {
+        const main = document.querySelector('main');
+        main.innerHTML = `
+            <article>
+                <h2>${post.title}</h2>
+                <img src="${post.img}" alt="${post.title}">
+                <p>By ${post.author} on ${post.date}</p>
+                <p>${post.content}</p>
+            </article>
+        `;
+    }
+}
+
 if (window.location.pathname.includes('blog.html')) {
-    loadPosts();
+    loadBlogCards();
+}
+
+if (window.location.pathname.includes('blog-details.html')) {
+    loadBlogDetails();
 }
